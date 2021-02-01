077eb3a061b3c7f935c2fea08ab25fe4087a9cc6
christophercampbell <chris.campbell1184@gmail.com>
2021-02-01 11:27:55 -0500

    flipped frames

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,56 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-	Rigidbody2D rb; 
-	public float speed;
-	public Animator animator;
-	public bool isDead = false;
-	float moveBy;
-	public string facing = "Up";
-    // Start is called before the first frame update
-    void Start()
-    {
-       rb = GetComponent<Rigidbody2D>();
-    }
+	public Rigidbody2D rb; 
+	public float moveSpeed = 2f;
+	Vector2 movement;
+
+	public Animator animatorUp;
+	public Animator animatorDown;
+	public Animator animatorLeft;
+	public Animator animatorRight;
 
     // Update is called once per frame
     void Update()
     {
-        //if (!PauseMenuBehaviour.isPaused && !isDead){
+    	//Input
+    	movement.x = Input.GetAxisRaw("Horizontal");
+    	movement.y = Input.GetAxisRaw("Vertical");
 
-    		Move(); 
-	      	animator.SetFloat("Speed", Mathf.Abs(moveBy));
+    	
+    }
+
+    void FixedUpdate(){
+    	//Movement
+    	rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+    
+
+
+
+    		//Move(); 
+	      	//animator.SetFloat("Speed", Mathf.Abs(moveBy));
       		
     	//}
     	// if (isDead == true){
      //  		animator.SetBool("isDead", true);
      //        //SoundManagerScript.PlaySound("death");
      //  	}
-    }
 
-    void Move() { 
-	    float x = Input.GetAxisRaw("Horizontal"); 
-	    moveBy = x * speed; 
-	    rb.velocity = new Vector2(moveBy, rb.velocity.y); 
-	    // if(Input.GetKeyDown (KeyCode.RightArrow)){
-	    // 	transform.Rotate(0, 180, 0);
-	    // 	facing = "Right";
-	    // }
-	    // if(Input.GetKeyDown (KeyCode.LeftArrow) && !facing){
-	    // 	transform.Rotate(0, 180, 0);
-	    // 	facing = true;
-	    // }
+// void Move() { 
+// 	    //moveBy = Input.GetAxisRaw("Horizontal") * speed;  
 	    
-	}
+// 	    rb.velocity = new Vector2(moveBy, rb.velocity.y); 
+// 	    if(Input.GetKeyDown (KeyCode.RightArrow)){
+// 	    	animatorRight.SetFloat("Speed", Mathf.Abs(moveBy));
+// 	    }
+	    
+// 	}
 
-	void OnCollisionEnter2D (Collision2D collision)
-    {
-	   	if(collision.transform.tag == "enemy")
-        {
-              isDead = true;
-        }
 
-    }
+
+	// void OnCollisionEnter2D (Collision2D collision)
+ //    {
+	//    	if(collision.transform.tag == "enemy")
+ //        {
+ //              isDead = true;
+ //        }
+
+ //    }
 
     // void OnTriggerEnter2D(Collider2D other){
     // 	if(other.gameObject.CompareTag("coins")){
@@ -71,5 +78,4 @@ public class PlayerController : MonoBehaviour
     // 	// 	isDead = true;
     // 	// }
     // }
-}
 
