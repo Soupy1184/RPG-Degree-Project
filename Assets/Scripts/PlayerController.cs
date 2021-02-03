47619b0db4f224956ba728a127d72a598d3dd9b8
christophercampbell <chris.campbell1184@gmail.com>
2021-02-03 12:33:46 -0500

    attack/bedroom

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,113 +23,97 @@ public class PlayerController : MonoBehaviour
     	//Input
     	movement.x = Input.GetAxisRaw("Horizontal");
     	movement.y = Input.GetAxisRaw("Vertical");
-
-        if (movement.x != 0 || movement.y != 0)
-        {
-            if (movement.y < 0) //down movement
+        if (!isDead){
+            if (movement.x != 0 || movement.y != 0)
             {
-                controller.downDirection.SetActive(true);
-                controller.upDirection.SetActive(false);
-                controller.leftDirection.SetActive(false);
-                controller.rightDirection.SetActive(false);
+                if (movement.y < 0) //down movement
+                {
+                    controller.downDirection.SetActive(true);
+                    controller.upDirection.SetActive(false);
+                    controller.leftDirection.SetActive(false);
+                    controller.rightDirection.SetActive(false);
 
+                    animatorDown.SetFloat("Speed", movement.y);
+                }
+                else if (movement.y > 0) //up movement
+                {
+                    controller.downDirection.SetActive(false);
+                    controller.upDirection.SetActive(true);
+                    controller.leftDirection.SetActive(false);
+                    controller.rightDirection.SetActive(false);
+
+                    animatorUp.SetFloat("Speed", movement.y);
+                }
+                else if (movement.x > 0) //right movement
+                {
+                    controller.downDirection.SetActive(false);
+                    controller.upDirection.SetActive(false);
+                    controller.leftDirection.SetActive(false);
+                    controller.rightDirection.SetActive(true);
+
+                    animatorRight.SetFloat("Speed", movement.x);
+                }
+                else if (movement.x < 0) //left movement
+                {
+                    controller.downDirection.SetActive(false);
+                    controller.upDirection.SetActive(false);
+                    controller.leftDirection.SetActive(true);
+                    controller.rightDirection.SetActive(false);
+
+                    animatorLeft.SetFloat("Speed", movement.x * -1);
+                }
+            }
+            else{
                 animatorDown.SetFloat("Speed", movement.y);
-            }
-            else if (movement.y > 0) //up movement
-            {
-                controller.downDirection.SetActive(false);
-                controller.upDirection.SetActive(true);
-                controller.leftDirection.SetActive(false);
-                controller.rightDirection.SetActive(false);
-
                 animatorUp.SetFloat("Speed", movement.y);
-            }
-            else if (movement.x > 0) //right movement
-            {
-                controller.downDirection.SetActive(false);
-                controller.upDirection.SetActive(false);
-                controller.leftDirection.SetActive(false);
-                controller.rightDirection.SetActive(true);
-
                 animatorRight.SetFloat("Speed", movement.x);
+                animatorLeft.SetFloat("Speed", movement.x);
             }
-            else if (movement.x < 0) //left movement
-            {
-                controller.downDirection.SetActive(false);
-                controller.upDirection.SetActive(false);
-                controller.leftDirection.SetActive(true);
-                controller.rightDirection.SetActive(false);
 
-                animatorLeft.SetFloat("Speed", movement.x * -1);
+            // Check keys for actions and use appropiate function
+            //
+            if (Input.GetKey(KeyCode.Space)){  // SWING ATTACK
+                animatorDown.SetTrigger("spaceKey");
+                animatorUp.SetTrigger("spaceKey");
+                animatorRight.SetTrigger("spaceKey");
+                animatorLeft.SetTrigger("spaceKey");
             }
         }
         else{
-            animatorDown.SetFloat("Speed", movement.y);
-            animatorUp.SetFloat("Speed", movement.y);
-            animatorRight.SetFloat("Speed", movement.x);
-            animatorLeft.SetFloat("Speed", movement.x);
-        }
-
-        if (isDead == true){
-      		animatorDown.SetBool("isDead", true);
+            animatorDown.SetBool("isDead", true);
             animatorUp.SetBool("isDead", true);
             animatorRight.SetBool("isDead", true);
             animatorLeft.SetBool("isDead", true);
             //SoundManagerScript.PlaySound("death");
-      	}
-
-
+        }
     }
 
-    void FixedUpdate(){
-    	//Movement
+    void FixedUpdate(){ //Movement
     	rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    void OnCollisionEnter2D (Collision2D collision){
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+    	
+    }
 }
-    
+
+// if(other.gameObject.CompareTag("coins")){
+    	// 	Destroy(other.gameObject);
+    	// }
+    	// if(other.gameObject.CompareTag("chest")){
+    	// 	SceneManager.LoadScene("BetweenLevel");
+    	// }
+    	// if(other.gameObject.CompareTag("chest2")){
+    	// 	SceneManager.LoadScene("GameOver");
+    	// 	PersistentManagerScript.Instance.Value = 0;
+    	// }
 
 
 
-    		//Move(); 
-	      	//animator.SetFloat("Speed", Mathf.Abs(moveBy));
-      		
-    	//}
-    
 
-// void Move() { 
-// 	    //moveBy = Input.GetAxisRaw("Horizontal") * speed;  
-	    
-// 	    rb.velocity = new Vector2(moveBy, rb.velocity.y); 
-// 	    if(Input.GetKeyDown (KeyCode.RightArrow)){
-// 	    	animatorRight.SetFloat("Speed", Mathf.Abs(moveBy));
-// 	    }
-	    
-// 	}
-
-
-
-	// void OnCollisionEnter2D (Collision2D collision)
- //    {
-	//    	if(collision.transform.tag == "enemy")
- //        {
- //              isDead = true;
- //        }
-
- //    }
-
-    // void OnTriggerEnter2D(Collider2D other){
-    // 	if(other.gameObject.CompareTag("coins")){
-    // 		Destroy(other.gameObject);
-    // 	}
-    // 	if(other.gameObject.CompareTag("chest")){
-    // 		SceneManager.LoadScene("BetweenLevel");
-    // 	}
-    // 	if(other.gameObject.CompareTag("chest2")){
-    // 		SceneManager.LoadScene("GameOver");
-    // 		PersistentManagerScript.Instance.Value = 0;
-    // 	}
-    // 	// if(other.gameObject.CompareTag("enemy")){
-    // 	// 	isDead = true;
-    // 	// }
-    // }
+  
 
