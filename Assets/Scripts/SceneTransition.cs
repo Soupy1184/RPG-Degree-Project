2b386ca54ea57a4a107ace392e01d6193ee8f52e
christophercampbell <chris.campbell1184@gmail.com>
2021-02-05 13:45:38 -0500

    transitions

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -7,7 +7,6 @@ public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
 
-    
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger){
