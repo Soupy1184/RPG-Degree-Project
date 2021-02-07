b71f5d3fb6f7a3920e98f86d36244cdf60f75c25
Zachary Moorman <zack_moorman@hotmail.com>
2021-02-07 14:03:09 -0500

    improve animation (state) management

--- Assets/Sidescrolling/Scripts/Sidescrolling_PlayerController.cs
+++ Assets/Sidescrolling/Scripts/Sidescrolling_PlayerController.cs
@@ -7,6 +7,8 @@ public class Sidescrolling_PlayerController : MonoBehaviour
 
      private Rigidbody2D rb;
      private Animator anim;
+     private enum State { idle, running, jumping }
+     private State state = State.idle;
 
     // Start is called before the first frame update
      private void Start()
@@ -18,23 +20,42 @@ public class Sidescrolling_PlayerController : MonoBehaviour
     // Update is called once per frame
      private void Update()
      {
+
+
           float hDirection = Input.GetAxis("Horizontal");
 
           if (hDirection < 0) {
                rb.velocity = new Vector2(-5, rb.velocity.y);
                transform.localScale = new Vector2(-1, 1);
-               anim.SetBool("Running", true);
           }
           else if (hDirection > 0) {
                rb.velocity = new Vector2(5, rb.velocity.y);
                transform.localScale = new Vector2(1, 1);
-               anim.SetBool("Running", true);
           }
           else {
-               anim.SetBool("Running", false);
+
           }
-          if (Input.GetKey(KeyCode.Space)) {
-               rb.velocity = new Vector2(rb.velocity.x, 8);
+
+          if (Input.GetButtonDown("Jump")) {
+               rb.velocity = new Vector2(rb.velocity.x, 8f);
+               state = State.jumping;
+          }
+          VelocityState();
+          anim.SetInteger("state", (int)state);
+     }
+
+
+     private void VelocityState() {
+          if(state == State.jumping) {
+
+          }
+
+          else if (Mathf.Abs(rb.velocity.x) > 2f) {
+               state = State.running;
+          }
+
+          else {
+               state = State.idle;
           }
      }
 }
