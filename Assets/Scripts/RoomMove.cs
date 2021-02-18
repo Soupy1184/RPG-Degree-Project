e128956eba3251d121b89fdd8decb55fdb3b2b60
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-02-17 22:15:34 -0500

    polygon collider for each room

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class RoomMove : MonoBehaviour
 {
-    public Vector2 cameraChangeMax;
-    public Vector2 cameraChangeMin;
+    // public Vector2 cameraChangeMax;
+    // public Vector2 cameraChangeMin;
     public Vector3 playerChange;
     private CameraFollow cam;
     // Start is called before the first frame update
@@ -23,10 +23,10 @@ public class RoomMove : MonoBehaviour
     private void OnTriggerEnter2D (Collider2D other){
         if (other.CompareTag("Player"))
         {
-            cam.minPosition.x = cameraChangeMin.x;
-            cam.minPosition.y = cameraChangeMin.y;
-            cam.maxPosition.x = cameraChangeMax.x;
-            cam.maxPosition.y = cameraChangeMax.y;
+            // cam.minPosition.x = cameraChangeMin.x;
+            // cam.minPosition.y = cameraChangeMin.y;
+            // cam.maxPosition.x = cameraChangeMax.x;
+            // cam.maxPosition.y = cameraChangeMax.y;
             other.transform.position += playerChange;
         }
     }
