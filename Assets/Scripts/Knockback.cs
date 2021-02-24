88ef05de5ffeae10dbce73f10074eb6c7f5ee1b4
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-02-23 21:57:05 -0500

    enemy knockback

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    // amount of force the player will give
+    public float thrust;
+    // to have the knockback to last
+    public float knockTime;
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            // turn kinematic to dynamic, at force, and turn back to kinematic
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.isKinematic = false;
+
+                //find difference between player and enemy
+                Vector2 difference = enemy.transform.position - transform.position;
+
+                //normalize difference 
+                difference = difference.normalized * thrust;
+
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+
+                // start back coroutine
+                StartCoroutine(KnockCo(enemy));
+            }
+            
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy){
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+
+            // velocity to 0
+            enemy.velocity = Vector2.zero;
+
+            // setting back to kinematic
+            enemy.isKinematic = true;
+        }
+    }
+}
