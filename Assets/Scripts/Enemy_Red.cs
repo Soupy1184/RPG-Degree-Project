f126d37485faf7a7e7a10f00b94356595e14f6de
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-02-28 12:56:49 -0500

    enemy red script update

--- Assets/Scripts/Enemy_Red.cs
+++ Assets/Scripts/Enemy_Red.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class Enemy_Red : Enemy
 {
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = EnemyState.idle;
+        //reference of rigidbody
+        myRigidbody = GetComponent<Rigidbody2D>();
+        //setting anim to be the animator conponent
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckDistance();
+    }
+
+    void CheckDistance(){
+        if (Vector3.Distance(target.position, 
+                            transform.position) <= chaseRadius 
+            && Vector3.Distance(target.position, 
+                            transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle 
+                    || currentState == EnemyState.walk
+                    && currentState != EnemyState.stagger)
+            {
+                
+            
+                // create temporrary vector2
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+                //moving the enemy
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+                anim.SetBool("awake", true);
+            }
+        } else if (Vector3.Distance(target.position, 
+                            transform.position) > chaseRadius){
+            anim.SetBool("awake", false);
+        }
+    }
+
+    // setting float to move directions
+    private void SetAnimFloat(Vector2 setVector){
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void changeAnim(Vector2 direction){
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y) )
+        {
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }   
+        }
+    }
+
+    private void ChangeState(EnemyState newState){
+        if (currentState != newState){
+            currentState = newState;
+        }
     }
 }
