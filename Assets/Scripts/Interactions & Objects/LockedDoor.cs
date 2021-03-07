17a6b5c6b21394e5eb4ece08c5d56667a071019d
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-03-06 21:39:32 -0500

    update locked door script

--- Assets/Scripts/Interactions & Objects/LockedDoor.cs
+++ Assets/Scripts/Interactions & Objects/LockedDoor.cs
@@ -1,18 +1,52 @@
+/* 
+    This script is for loocked doors in Level 1
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LockedDoor : MonoBehaviour
+// for now the door will only recquire keys
+// if there is extra time to add, 
+// could add more doors with different opening methods
+// like enemy and button
+public enum DoorType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    key,
+    enemy, 
+    button
+}
+
+public class LockedDoor : Dialog
+{
+    [Header("Door variables")]
+    public DoorType thisDoorType;
+    public bool open = false;
+    public Inventory playerInventory;
+    // public SpriteRenderer doorSprite;
+    public BoxCollider2D physicsCollider;
+
+    private void Update (){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(playerInRange && thisDoorType == DoorType.key){
+                //checks for key
+                if(playerInventory.numberOfKeys > 0){
+                    // remove a player key
+                    playerInventory.numberOfKeys--;
+                    Open();
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Open(){
+        // set open to true
+        open = true;
+        // turn off door's box collider
+        physicsCollider.enabled = false;
+    }
+
+    public void Close(){
+
     }
 }
