51ae5df53631b83f2c379c8e17e48cde088ebde6
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-03-13 21:00:03 -0500

    before new enemy attack anim

--- Assets/Scripts/Inventory/HealthReaction.cs
+++ Assets/Scripts/Inventory/HealthReaction.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class HealthReaction : MonoBehaviour
 {
-    public FloatValue playeHealth;
-    public SignalSender healthSignal;
+    // public FloatValue playeHealth;
+    // public SignalSender healthSignal;
     
-    public void Use(int amountToIncrease){
-        playerHealth.RuntimeValue += amountToIncrease;
-        healthSignal.Raise();
-    }
+    // public void Use(int amountToIncrease){
+    //     playerHealth.RuntimeValue += amountToIncrease;
+    //     healthSignal.Raise();
+    // }
 }
