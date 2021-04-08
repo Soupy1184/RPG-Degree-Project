cb4d0c8b44e4977af79beee266b7307b555bf701
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-04-08 16:20:26 -0400

    Update RoomMove.cs

--- Assets/Scripts/Game/RoomMove.cs
+++ Assets/Scripts/Game/RoomMove.cs
@@ -1,3 +1,10 @@
+/*
+    written by: Afieqha Mieza
+    date: January 2021
+*/
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +13,13 @@ public class RoomMove : MonoBehaviour
 {
     // take position vector of change of position
     public Vector3 playerChange;
-
+    // reference to camera
     private CameraFollow cam;
     
     // Start is called before the first frame update
     void Start()
     {
+        // get main camera
         cam = Camera.main.GetComponent<CameraFollow>();
     }
 
@@ -23,7 +31,7 @@ public class RoomMove : MonoBehaviour
 
     // when player's position collide with the door 
     // move the player to next room
-    // at a position
+    // at a position (based on playerChange)
     private void OnTriggerEnter2D (Collider2D other){
         if (other.CompareTag("Player"))
         {
