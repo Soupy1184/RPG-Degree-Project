b902454a070ad857a3643979b6e0971d04792dc5
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-04-08 16:48:48 -0400

    Update Coin.cs

--- Assets/Scripts/Interactions & Objects/Coin.cs
+++ Assets/Scripts/Interactions & Objects/Coin.cs
@@ -1,14 +1,24 @@
+/*
+    written by: Afieqha Mieza
+    date: March 2021
+    function: script to collect coins
+*/
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : Powerup
 {
+    [Header("Player Inventory")]
     public Inventory playerInventory;
     public bool coinCounted;
-    private GameObject coinSound;
+    
 
     // [Header("Sound Effects")]
+    private GameObject coinSound;
     private SfxManager sfxMan;
 
     // Start is called before the first frame update
@@ -19,6 +29,7 @@ public class Coin : Powerup
             coinSound = GameObject.Find("coinCollect");
         }
 
+        // reference to sound effect script
         sfxMan = FindObjectOfType<SfxManager>();
     }
 
@@ -30,16 +41,25 @@ public class Coin : Powerup
 
     //add a coin, send the signal to the inventory, destroy the object
     void OnTriggerEnter2D(Collider2D other){
+        // if collides with player and not yet counted
         if(other.CompareTag("Player") && !coinCounted){
-               if (GameObject.Find("coinCollect") != null) {
-                    coinSound.GetComponent<AudioSource>().Play();
-               }
+            if (GameObject.Find("coinCollect") != null) {
+                coinSound.GetComponent<AudioSource>().Play();
+            }
+
+            // update coin in inventory
             playerInventory.coins += 1;
+
+            // play sound effect
             sfxMan.coins.Play();
+
+            // raise signal
             powerupSignal.Raise();
-            // Destroy(this.gameObject);
+
+            // set object to inactive once collected
             this.gameObject.SetActive(false);
-            Debug.Log(other + "coin");
+
+            // refer to coin is already counted
             coinCounted = true;
         }
         
