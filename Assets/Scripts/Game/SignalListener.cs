28a8ff20d5f1ce7e13855206687706b1068ee8a6
Afieqha Mieza <afieqhaazemi@gmail.com>
2021-04-08 16:20:30 -0400

    Update SignalListener.cs

--- Assets/Scripts/Game/SignalListener.cs
+++ Assets/Scripts/Game/SignalListener.cs
@@ -1,3 +1,11 @@
+/*
+    written by: Afieqha Mieza
+    date: January 2021
+    signaling concept resource: https://www.youtube.com/watch?v=BLfNP4Sc_iA
+*/
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +13,21 @@ using UnityEngine.Events;
 
 public class SignalListener : MonoBehaviour
 {
+    // reference to a signal
     public SignalSender signal;
     public UnityEvent signalEvent;
 
-    // called when signal is raised
+    // invoke event when signal is raised
     public void OnSignalRaised(){
         signalEvent.Invoke();
     }
 
-    //go to signal and register
+    // registering signal
     private void OnEnable(){
         signal.RegisterListener(this);
     }
 
+    // disable signal
     private void OnDisable(){
         signal.DeRegisterListener(this);
     }
